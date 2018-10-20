b5bce9833c1856aec6ada7f65cc2eb585e55c4bf
Amogh Gupta <gupta.amogh15@gmail.com>
2018-10-20 17:03:32 +0530

    Commit react learnings

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
+import Radium, {StyleRoot} from 'radium';
 //Simple
 // state means data inside a component
 // props means data outside the component
@@ -19,8 +20,10 @@ class App extends Component {
       {name:'Ram', age:'28'},
       {name:'Shyam', age:'27'},
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
+
   // if we use the arrow function then 'this' will work easily as it will refer to the this class
   // meaning the enclosing function
   switchName = (value)=>{
@@ -34,52 +37,109 @@ class App extends Component {
       ]
     })
   }
- 
-  nameChangedHandler = (event)=>{
-    this.setState({
-      person:[
-        {name:'Amogh1', age:'26'},
-        {name: event.target.value, age:'28'},
-        {name:'Shyam1', age:'27'},
-      ]
-    })
-  }
-  // but when we use normal functions we have to bind this at the time when constructor is created
+
+ // but when we use normal functions we have to bind this at the time when constructor is created
   // because in normal functions this would get undefined
   // switchName = function(){
   //   // this is how we manipulate state
   //   console.log(this.state);
   // }
 
+  nameChangedHandler = (event,index)=>{
+    const person = {
+      ...this.state.person[index]
+    }
+    person.name = event.target.value;
+    const persons = [...this.state.person];
+    persons[index] = person;
+   
+    this.setState({
+      person:persons
+    })
+  }
+
+  togglePersonsHandler = ()=>{
+    
+    this.setState({
+      showPersons: !this.state.showPersons
+    })
+  }
+  
+  deletePersonHandler = (index)=>{
+    const persons = this.state.person.slice();
+    persons.splice(index,1);
+    this.setState({
+      person: persons
+    })
+  }
+
   render() {
     // giving inline styles
     const style = {
-      color: 'red'
+     backgroundColor: 'green',
+     color: 'white',
+     border: '1px solid blue',
+     padding: '8px',
+     cursor: 'pointer',
+     ':hover':{
+       backgroundColor: 'lightgreen',
+       color: 'black'
+     }
     }
-    return (
-      <div className="App">
-         <h1 style={style}>I am a react app</h1>
 
-          {/* <button onClick={this.switchName()}>Switch Name</button> */}
-          {/* we can pass the function reference as prop to the child so that 
-          we can execute the same function from child component */}
-          <button onClick={this.switchName}>Switch Name Way1</button>
+    let persons = null;
 
-         <Person name={this.state.person[0].name} 
+    if(this.state.showPersons){
+      style.backgroundColor = 'red';
+      style[':hover']={
+        backgroundColor: 'lightred',
+        color: 'black'
+      }
+      persons = (
+        <div>
+          {this.state.person.map((person,index)=>{
+            return <Person name={person.name} 
+            age={person.age}
+            click = {()=>this.deletePersonHandler(index)}
+            key={index}
+            changed={(event)=>this.nameChangedHandler(event,index)}></Person>
+          })}
+            {/* <Person name={this.state.person[0].name} 
           age={this.state.person[0].age} 
           changeName={this.switchName}>I am content inside person directive</Person>
           
           <Person name={this.state.person[1].name} 
           age={this.state.person[1].age} 
+          //changeName={this.switchName('hello')}
           changeName={this.switchName.bind(this,'hello')}
           changed={this.nameChangedHandler}>Person1 para</Person>
 
-          <button onClick={()=>this.switchName('hello')}>Switch Name Way2</button>
+          <button onClick={()=>this.switchName('hello')}>Switch Name Way2</button> */}
+        </div>
+      );
 
-       
+    }
+    const classes = [];
+    if(this.state.person.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.person.length <= 1){
+      classes.push('bold');
+    }
+    return (
+      <StyleRoot>
+      <div className="App">
+         <h1>I am a react app</h1>
+         <p className={classes.join(' ')}>This is working</p>
+          {/* <button onClick={this.switchName()}>Switch Name</button> */}
+          {/* we can pass the function reference as prop to the child so that 
+          we can execute the same function from child component */}
+          <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+         {persons}
       </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default  Radium(App);
--- my-react-project/src/Person/Person.js
+++ my-react-project/src/Person/Person.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import Radium from 'radium';
 import './Person.css';
 
 
 //stateless component
 const Person = (props)=>{
-
+    const style = {
+        '@media (min-width: 500px)':{
+            width: '450px'
+        }
+    };
 
     // dynamic JSX is wrapped in single curly braces
-    return <div className="Person">
+    return <div className="Person" style={style}>
             {/* <h1>I am from Person and {Math.random()*30} years old</h1> */}
-            <h1>I am {props.name} and my age is {props.age}</h1>
+            <h1 onClick={props.click}>I am {props.name} and my age is {props.age}</h1>
             {/* this is how we access any element between openeing and closing tag of our element */}
-            <p onClick={props.changeName}>{props.children}</p>
+            <p onClick={props.click}>{props.children}</p>
             {/* 2 way binding */}
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
 }
     
-export default Person;
+export default Radium(Person);
